2a62e5a4ba5fee88fc406a963863989705650b59
unknown <pc23@pc18.inovasisolusi.com>
2019-12-19 10:57:32 +0700

    adding react local(current) & docker setup

--- process.sh
+++ process.sh
@@ -1,4 +1,6 @@
-pm2 start ./test-eg2/server.js
+pm2 start ./test-eg2/server.js --name "eg"
 
-pm2 start ./api-auth/app.js
-pm2 start ./api-crud/app.js
+pm2 start ./api-auth/app.js --name "api-auth"
+pm2 start ./api-crud/app.js --name "api-crud"
+
+cd ./0-react-client/ && npm run start
--- test-eg2/server.js
+++ test-eg2/server.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const gateway = require('express-gateway');
 
-console.log("Masukk");
 
 gateway()
   .load(path.join(__dirname, 'config'))
